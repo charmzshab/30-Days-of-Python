7e1786ab70f7f2b64ecd8733beb04e6baaaaa338
shabix <kmharouna@gmail.com>
2020-07-07 11:31:15 +0300

    worked on internal and external imports on Day 8

--- Day 8/send.py
+++ Day 8/send.py
@@ -1,13 +1,30 @@
+import sys
+import requests
 from datetime import datetime
 from formatting import format_msg
 
 
-def send(name, website=None):
+def send(name, website=None, verbose=False):
     if website != None:
         msg = format_msg(name=name, website=website)
     else:
         msg = format_msg(name=name)
-
+    if verbose:
+        print(name, website)
     # send the message
+    r = requests.get("http://httpbin.org/json")
+    if r.status_code == 200:
+        return r.json()
+    else:
+        return "There was an error"
 
     return msg
+
+
+if __name__ == "__main__":
+    print(sys.argv)
+    name = "Unknown"
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+    response = send(name, verbose=True)
+    print(response)
