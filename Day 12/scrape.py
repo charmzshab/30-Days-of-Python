e9777e90f8db228aac152b82ad07001f8e4d8d2f
shabix <kmharouna@gmail.com>
2020-07-14 17:56:36 +0300

    refactoring code

--- Day 12/scrape.py
+++ Day 12/scrape.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import datetime
 import requests
 import pandas as pd
@@ -21,12 +22,14 @@ def url_to_txt(url, file_name="world.html", save=False, name=None):
             with open(f"world-{name}.html", "w") as f:
                 f.write(html_text)
         return html_text
-    return ""
+    return None
 
 
 def parse_and_extract(url, name="2020"):
 
     html_text = url_to_txt(url, name)
+    if html_text == None:
+        return
     r_html = HTML(html=html_text)  # formats the returned html response in to basic html
     table_class = ".imdb-scroll-table"  # html element selector
     # table_class = "#table"
@@ -35,37 +38,38 @@ def parse_and_extract(url, name="2020"):
     # print(r_table)
     table_data = []
     header_names = []
-    if len(r_table) == 1:
+    if len(r_table) == 0:
+        return False
         # print(r_table[0].text)
-        parsed_table = r_table[0]
-        rows = parsed_table.find("tr")  # gets all the table rows --> this is a list
-        header_row = rows[
-            0
-        ]  # retrieves the table header which is the first element in the list
-        header_cols = header_row.find("th")
-        header_names = [name.text for name in header_cols]
+    parsed_table = r_table[0]
+    rows = parsed_table.find("tr")  # gets all the table rows --> this is a list
+    header_row = rows[
+        0
+    ]  # retrieves the table header which is the first element in the list
+    header_cols = header_row.find("th")
+    header_names = [name.text for name in header_cols]
 
-        for row in rows[
-            1:
-        ]:  # get the remaining row that contain the data without the header
-            # print(row.text)
-            cols = row.find("td")  # gets all the column cells per row
-            row_data = []
-            for i, col in enumerate(cols):
-                # print(i, col.text, "\n\n")
-                row_data.append(col.text)
-            table_data.append(row_data)
-        df = pd.DataFrame(table_data, columns=header_names)
-        path = os.path.join(BASE_DIR, "data")
-        os.makedirs(path, exist_ok=True)
-        filepath = os.path.join("data", f"{name}.csv")
-
-        df.to_csv(filepath, index=False)
+    for row in rows[
+        1:
+    ]:  # get the remaining row that contain the data without the header
+        # print(row.text)
+        cols = row.find("td")  # gets all the column cells per row
+        row_data = []
+        for _, col in enumerate(cols):
+            # print(i, col.text, "\n\n")
+            row_data.append(col.text)
+        table_data.append(row_data)
+    df = pd.DataFrame(table_data, columns=header_names)
+    path = os.path.join(BASE_DIR, "data")
+    os.makedirs(path, exist_ok=True)
+    filepath = os.path.join("data", f"{name}.csv")
+    df.to_csv(filepath, index=False)
+    return True
     # print(header_names)
     # print(table_data)
 
 
-def run(start_year=None, years_ago=10):
+def run(start_year=None, years_ago=0):
     if start_year == None:
         now = datetime.datetime.now()
         start_year = now.year
@@ -73,12 +77,25 @@ def run(start_year=None, years_ago=10):
     assert isinstance(years_ago, int)
     assert len(f"{start_year}") == 4
     for i in range(0, years_ago + 1):
-        url = f"https://www.boxofficemojo.com/year/world/{start_year}"
-        parse_and_extract(url, name=start_year)
-        print(f"Finished {start_year}")
+        url = f"https://www.boxofficemojo.com/year/world/{start_year}/"
+        finished = parse_and_extract(url, name=start_year)
+        if finished:
+            print(f"Finished {start_year}")
+        else:
+            print(f"{start_year} not finished")
         start_year -= 1
 
 
 if __name__ == "__main__":
-    run()
+    # sys.argv[1] & sys.argv[2] are commandline inputs
+    try:
+        start = int(sys.argv[1])
+    except:
+        start = None
+    try:
+        duration = int(sys.argv[2])
+    except:
+        duration = 0
+
+    run(start_year=start, years_ago=duration)
 
